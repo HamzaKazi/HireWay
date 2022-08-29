884153366c0375647e00709c4974f520c9c2648e
hamed aljamry <hamed970@hotmail.com>
2022-08-29 17:33:48 +0100

    seed fixed

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,6 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
-  validates :type, presence: true
+  validates :name, presence: true
   validates :description, presence: true
   validates :category, presence: true
   validates :category, inclusion: {
