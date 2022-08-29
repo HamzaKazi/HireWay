9d01f022e624d8d6f571cf44587047c3c8bb2531
Bilal Nakhuda <bilal_nakhuda@hotmail.co.uk>
2022-08-29 15:56:56 +0100

    bilals file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :boats do
+    resources :bookings, only: [ :new, :create ]
+    resources :reviews, only: [ :new, :create ]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
