b8b7b44cea3a2984566946408b4268c830ab345c
hamed aljamry <hamed970@hotmail.com>
2022-09-02 12:33:38 +0100

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_01_111914) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,10 +73,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_111914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.string "image"
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-    t.string "image"
     t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
